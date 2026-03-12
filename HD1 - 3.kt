fcb14af8839f605efb3dcc02e930693ca841ceb9
mayaraellenn-06 <mayara.ellenn@gmail.com>
2026-03-12 10:37:00 -0300

    Add files via upload

---
+++ HD1 - 3.kt
@@ -0,0 +1,9 @@
+fun main() {
+    print("Olá,Mdigite o seu nome: ")
+    val nomeDoUsuario = readln()
+
+    print("Digite a sua idade: ")
+    val idade = readln().toInt()
+
+    println("Olá, $nomeDoUsuario, sua idade é $idade")
+}
